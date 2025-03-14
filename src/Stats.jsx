a3b921e3f11e33a2ae3fdc9196c9026db69a269a
MarcosPerez16 <marcos.perez06@yahoo.com>
2025-03-13 19:08:59 -0700

    Made the stats component a reusable component by creating a stat component inside and using props to display data.

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,5 +1,21 @@
 const Stats = () => {
-  return <section className='stats'>Stats</section>;
+  return (
+    <section className='stats'>
+      <Stat number={0} label='Words' />
+      <Stat number={0} label='Characters' />
+      <Stat number={280} label='Instagram' />
+      <Stat number={2200} label='Facebook' />
+    </section>
+  );
+};
+
+const Stat = ({ number, label }) => {
+  return (
+    <section className='stat'>
+      <span className='stat__number'>{number}</span>
+      <h2 className='second-heading'>{label}</h2>
+    </section>
+  );
 };
 
 export default Stats;
